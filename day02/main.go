a8ca3e7a8d4b8c05dafbfa33d46a7bc878f0fc16
Kyle Kizirian <kyle.kizirian@gmail.com>
2022-12-02 07:46:20 -0500

    day 2 part 2

--- day02/main.go
+++ day02/main.go
@@ -20,17 +20,14 @@ const (
 	DRAW
 )
 
-type Round struct {
-	Opponent RockPaperScissors
-	You      RockPaperScissors
-}
-
 var (
 	letterToRPS = map[string]RockPaperScissors{
 		"A": ROCK, "X": ROCK,
 		"B": PAPER, "Y": PAPER,
 		"C": SCISSORS, "Z": SCISSORS,
 	}
+
+	letterToOutcome = map[string]WinLoseDraw{"X": LOSE, "Y": DRAW, "Z": WIN}
 
 	rpsToPoints = map[RockPaperScissors]int{ROCK: 1, PAPER: 2, SCISSORS: 3}
 	wldToPoints = map[WinLoseDraw]int{LOSE: 0, DRAW: 3, WIN: 6}
@@ -47,12 +44,42 @@ var (
 	}
 )
 
-func main() {
-	rounds := readInput()
-	part1(rounds)
+type Line struct {
+	First  string
+	Second string
 }
 
-func part1(rounds []Round) {
+type Round struct {
+	Opponent RockPaperScissors
+	You      RockPaperScissors
+}
+
+func main() {
+	lines := readInput()
+	part1(lines)
+	part2(lines)
+}
+
+func part1(lines []Line) {
+	rounds := make([]Round, len(lines))
+	for i, line := range lines {
+		rounds[i] = line.toRoundPart1()
+	}
+
+	var points int
+	for _, round := range rounds {
+		points += calculatePoints(round)
+	}
+
+	fmt.Println("part 1: ", points)
+}
+
+func part2(lines []Line) {
+	rounds := make([]Round, len(lines))
+	for i, line := range lines {
+		rounds[i] = line.toRoundPart2()
+	}
+
 	var points int
 	for _, round := range rounds {
 		points += calculatePoints(round)
@@ -76,23 +103,50 @@ func getOutcome(round Round) WinLoseDraw {
 	return LOSE
 }
 
-func readInput() []Round {
+func (l Line) toRoundPart1() Round {
+	return Round{
+		Opponent: letterToRPS[l.First],
+		You:      letterToRPS[l.Second],
+	}
+}
+
+func (l Line) toRoundPart2() Round {
+	var you RockPaperScissors
+	opponent := letterToRPS[l.First]
+	outcome := letterToOutcome[l.Second]
+
+	switch outcome {
+	case DRAW:
+		you = opponent
+	case LOSE:
+		you = toLosing[opponent]
+	case WIN:
+		you = toWinning[opponent]
+	}
+
+	return Round{
+		Opponent: opponent,
+		You:      you,
+	}
+}
+
+func readInput() []Line {
 	file, err := os.Open("input.txt")
 	checkErr(err)
 
 	scanner := bufio.NewScanner(file)
 	scanner.Split(bufio.ScanLines)
 
-	var rounds []Round
+	var lines []Line
 	for scanner.Scan() {
 		choices := strings.Split(scanner.Text(), " ")
-		rounds = append(rounds, Round{
-			Opponent: letterToRPS[choices[0]],
-			You:      letterToRPS[choices[1]],
+		lines = append(lines, Line{
+			First:  choices[0],
+			Second: choices[1],
 		})
 	}
 
-	return rounds
+	return lines
 }
 
 func checkErr(err error) {
