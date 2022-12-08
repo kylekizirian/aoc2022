e2c639d4d36534c38fe4bd077ad6f379a9573431
Kyle Kizirian <kyle.kizirian@gmail.com>
2022-12-08 04:19:47 -0500

    day 8 part 2

--- day08/day08.py
+++ day08/day08.py
@@ -49,6 +49,47 @@ def part1(heights: List[List[int]]):
     print(f"part 1: {num_visible}")
 
 
+def part2(heights: List[List[int]]):
+    scores = [
+        [scenic_score(i, j, heights) for i in range(len(heights[j]))]
+        for j in range(len(heights))
+    ]
+    max_score = 0
+    for row in scores:
+        max_score = max(max_score, max(row))
+
+    print(f"part 2: {max_score}")
+
+
+def scenic_score(i: int, j: int, heights: List[List[int]]) -> int:
+    if i == 0 or j == 0 or i == len(heights) - 1 or j == len(heights[i]) - 1:
+        return 0
+
+    height = heights[i][j]
+    above = below = left = right = 0
+    for row in range(i - 1, -1, -1):
+        above += 1
+        if heights[row][j] >= height:
+            break
+
+    for row in range(i + 1, len(heights)):
+        below += 1
+        if heights[row][j] >= height:
+            break
+
+    for col in range(j - 1, -1, -1):
+        left += 1
+        if heights[i][col] >= height:
+            break
+
+    for col in range(j + 1, len(heights[i])):
+        right += 1
+        if heights[i][col] >= height:
+            break
+
+    return above * below * left * right
+
+
 if __name__ == "__main__":
     heights = []
     with open(INPUT_FILE) as f:
@@ -56,3 +97,4 @@ if __name__ == "__main__":
             heights += [[int(height) for height in row.strip()]]
 
     part1(heights)
+    part2(heights)
